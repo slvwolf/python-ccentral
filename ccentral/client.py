387eb4d0a9cd316591ed0efb35c2b49ed9ec97af
Santtu Järvi <santtu@unity3d.com>
2016-11-02 10:48:19 +0200

    Version 0.3.2

--- ccentral/client.py
+++ ccentral/client.py
@@ -14,7 +14,7 @@ from etcd import Client, EtcdKeyNotFound, EtcdException
 _log = logging.getLogger("ccentral")
 
 TTL_DAY = 24 * 60 * 60
-VERSION = "0.3.1"
+VERSION = "0.3.2"
 API_VERSION = "1"
 
 class IncCounter:
